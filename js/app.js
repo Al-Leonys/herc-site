7790042a8f0a6cc4465f85750a22f7cf08f95e00
Alex Shaigorodsky <ashaigor@gmail.com>
2026-08-30 17:00:55 -0400

    fix javascript reference error

--- js/app.js
+++ js/app.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
     initSubsystemsCADAnimation();
     initTimelineScrollAnimation();
     initTelemetrySimulation();
-    initContactForm();
     initIntegratedGoogleForm();
     initPhoneNumberFormatter();
     initAccordionGallery();
